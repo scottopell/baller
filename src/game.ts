2175b44460d9b17068a00a125f416b1e37a55460
Scott Opell <me@scottopell.com>
2022-05-11 10:06:05 -0400

    Splits up giant touch handler

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,4 @@
 import { Board, BoardSizing } from "./boardsizing";
-interface Dot {
-    color: number,
-    isSelected: boolean
-};
 
 const k_GRID_SIZE = 15;
 
@@ -40,26 +36,75 @@ class Game {
         this.currentTouches = new Map();
         this.activeTouch = null;
 
-        canvas.addEventListener('touchstart', this.handleTouchStart.bind(this));
-        canvas.addEventListener('touchend', this.handleTouchEnd.bind(this));
-        canvas.addEventListener('touchcancel', this.handleTouchCancel.bind(this));
-        canvas.addEventListener('touchmove', this.handleTouchMove.bind(this));
+        const touchEventHandler = (e: TouchEvent) => {
+            e.preventDefault();
+            for (const touch of e.changedTouches) {
+                this.handleTouch(touch, e.type);
+            }
+        };
+        canvas.addEventListener('touchstart', touchEventHandler);
+        canvas.addEventListener('touchend', touchEventHandler);
+        canvas.addEventListener('touchcancel', touchEventHandler);
+        canvas.addEventListener('touchmove', touchEventHandler);
     }
 
     collapse() {
     }
 
-    highlightTouch(changedTouches: Array<Touch>, eventType: "start" | "move" | "end" | "cancel") {
-        const touchToRC = (touch: Touch) => {
-            const canvas = touch.target as HTMLCanvasElement;
-            const canvasBounds = canvas.getBoundingClientRect();
+    handleTouch(touch: Touch, eventType: string) {
+        if (eventType === "touchstart") {
+            this.handleStartTouch(touch);
+        } else if (eventType === "touchmove") {
+            this.handleMoveTouch(touch);
+        } else if (eventType === "touchend") {
+            this.handleEndTouch(touch);
+        } else if (eventType === "touchcancel") {
+            this.handleCancelTouch(touch);
+        } else {
+            throw new Error("Unknown touch type");
+        }
+        console.log(`Processed touch ${touch.identifier}. activeTouch is now ${this.activeTouch !== null ? "not" : ""} null.`);
+        if (this.activeTouch !== null) {
+            console.log(`After touch. tailR ${this.activeTouch.tailRC[0]} tailC ${this.activeTouch.tailRC[1]}`);
+        }
+    }
 
-            const canvasY = touch.pageY - canvasBounds.y;
-            const canvasX = touch.pageX - canvasBounds.x;
+    _touchToRC(touch: Touch) {
+        const canvas = touch.target as HTMLCanvasElement;
+        const canvasBounds = canvas.getBoundingClientRect();
 
-            return this.board.sizing.cartesianCoordToRowColumn(canvasX, canvasY);
+        const canvasY = touch.pageY - canvasBounds.y;
+        const canvasX = touch.pageX - canvasBounds.x;
+
+        return this.board.sizing.cartesianCoordToRowColumn(canvasX, canvasY);
+    };
+
+    handleStartTouch(touch: Touch) {
+        if (this.activeTouch !== null) {
+            // Have an existing activeTouch, but this is a start touch,
+            // so there must be a second finger
+            console.log("Got start event, but had previously active touch. Must be second finger");
+            return;
+        }
+
+        // New Touch
+        const rowColumnCoord = this._touchToRC(touch);
+        if (rowColumnCoord === null) {
+            // Out of bounds, no valid ball at coordinate
+            return;
+        }
+        const [r, c] = rowColumnCoord;
+        console.log(`Start. r ${r} c ${c}`);
+        this.activeTouch = {
+            touchId: touch.identifier,
+            startRC: [r, c],
+            tailRC: [r, c],
         };
+        const currentBall = this.board.get(r, c);
+        currentBall.isSelected = true;
+    }
 
+    handleMoveTouch(touch: Touch) {
         const isImmediateNeighbor = (pointOne: [number, number], pointTwo: [number, number]) => {
             const [r1, c1] = pointOne;
             const [r2, c2] = pointTwo;
@@ -70,109 +115,60 @@ class Game {
             return rowDistance === 1 && colDistance === 0 || colDistance === 1 && rowDistance === 0;
         };
 
-        for(let i = 0; i < changedTouches.length; i++) {
-            const touch = changedTouches[i];
-            if (eventType === "start") {
-                if (this.activeTouch !== null) {
-                    // Have an existing activeTouch, but this is a start touch,
-                    // so there must be a second finger
-                    console.log("Got start event, but had previously active touch. Must be second finger");
-                    continue;
-                }
+        if (this.activeTouch === null) {
+            console.error("Got move event but there was no active touch, wtf");
+            return;
+        }
+        if (this.activeTouch.touchId !== touch.identifier) {
+            // This is a second touch that has moved, not the active one, ignore it
+            console.log("Got move event, but for a non-active touch. Ignoring.");
+            return;
+        }
+        // Continuation of existing touch
+        const [ tailRow, tailColumn ] = this.activeTouch.tailRC;
+        const tailBall = this.board.get(tailRow, tailColumn);
+        const rowColumnCoord = this._touchToRC(touch);
+        if (rowColumnCoord === null) {
+            // Out of bounds, no valid ball at coordinate
+            return;
+        }
+        const [r, c] = rowColumnCoord;
+        const currentBall = this.board.get(r, c);
+        console.log(`Move.\n\tTailR: ${tailRow} TailC: ${tailColumn} TailColor: ${tailBall.color}\n\tr: ${r} c: ${c} color: ${currentBall.color}`);
 
-                // New Touch
-                const rowColumnCoord = touchToRC(touch);
-                if (rowColumnCoord === null) {
-                    // Out of bounds, no valid ball at coordinate
-                    continue;
-                }
-                const [r, c] = rowColumnCoord;
-                console.log(`Start. r ${r} c ${c}`);
-                this.activeTouch = {
-                    touchId: touch.identifier,
-                    startRC: [r, c],
-                    tailRC: [r, c],
-                };
-                const currentBall = this.board.get(r, c);
-                currentBall.isSelected = true;
-            } else if (eventType === "move") {
-                if (this.activeTouch === null) {
-                    console.error("Got move event but there was no active touch, wtf");
-                    continue;
-                }
-                if (this.activeTouch.touchId !== touch.identifier) {
-                    // This is a second touch that has moved, not the active one, ignore it
-                    console.log("Got move event, but for a non-active touch. Ignoring.");
-                    continue;
-                }
-                // Continuation of existing touch
-                const [ tailRow, tailColumn ] = this.activeTouch.tailRC;
-                const tailBall = this.board.get(tailRow, tailColumn);
-                const rowColumnCoord = touchToRC(touch);
-                if (rowColumnCoord === null) {
-                    // Out of bounds, no valid ball at coordinate
-                    continue;
-                }
-                const [r, c] = rowColumnCoord;
-                const currentBall = this.board.get(r, c);
-                console.log(`Move.\n\tTailR: ${tailRow} TailC: ${tailColumn} TailColor: ${tailBall.color}\n\tr: ${r} c: ${c} color: ${currentBall.color}`);
-
-                if (currentBall.isSelected) {
-                    // No backtracking on path selections
-                    continue;
-                }
-
-                // if currentball is one to left/right/down/up
-                // and currentball is same color
-
-                if (tailBall.color !== currentBall.color) {
-                    // Invalid move, not going to record anything;
-                    console.log(`Current is diff color from tail, continuing...`);
-                    continue;
-                }
-                if (isImmediateNeighbor([tailRow, tailColumn], [r, c])) {
-                    currentBall.isSelected = true;
-                    this.activeTouch.tailRC = [r, c];
-                } else {
-                    console.log(`Current is not immediate neighbor, continueing...`);
-                }
-            } else if (eventType === "end") {
-                if (this.activeTouch === null) {
-                    console.error("Got end event, but there was no active touch. Nothing to do.");
-                    continue;
-                }
-                this.collapse();
-                this.activeTouch = null;
-            } else if (eventType === "cancel") {
-                console.log("Cancelled touch");
-                this.activeTouch = null;
-            }
-            console.log(`After touch. activeTouch is ${this.activeTouch !== null ? "not" : ""} null.`);
-            if (this.activeTouch !== null) {
-                console.log(`After touch. tailR ${this.activeTouch.tailRC[0]} tailC ${this.activeTouch.tailRC[1]}`);
-            }
+        if (currentBall.isSelected) {
+            // No backtracking on path selections
+            return;
         }
 
+        // if currentball is one to left/right/down/up
+        // and currentball is same color
+
+        if (tailBall.color !== currentBall.color) {
+            // Invalid move, not going to record anything;
+            console.log(`Current is diff color from tail, continuing...`);
+            return;
+        }
+        if (isImmediateNeighbor([tailRow, tailColumn], [r, c])) {
+            currentBall.isSelected = true;
+            this.activeTouch.tailRC = [r, c];
+        } else {
+            console.log(`Current is not immediate neighbor, continueing...`);
+        }
     }
 
-    handleTouchStart(e: TouchEvent) {
-        e.preventDefault();
-        this.highlightTouch([...e.changedTouches], "start");
+    handleEndTouch(touch: Touch) {
+        if (this.activeTouch === null) {
+            console.error("Got end event, but there was no active touch. Nothing to do.");
+            return;
+        }
+        this.collapse();
+        this.activeTouch = null;
     }
 
-    handleTouchEnd(e: TouchEvent) {
-        e.preventDefault();
-        this.highlightTouch([...e.changedTouches], "end");
-    }
-
-    handleTouchCancel(e: TouchEvent) {
-        e.preventDefault();
-        this.highlightTouch([...e.changedTouches], "cancel");
-    }
-
-    handleTouchMove(e: TouchEvent) {
-        e.preventDefault();
-        this.highlightTouch([...e.changedTouches], "move");
+    handleCancelTouch(touch: Touch) {
+        console.log("Cancelled touch");
+        this.activeTouch = null;
     }
 
     gameLoop(timestamp: number) {
